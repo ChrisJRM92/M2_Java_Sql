88ef0d13cc71220b62e57923f2f8b938a140d61f
Christian R <cjromerom28@gmail.com>
2025-12-04 14:17:00 -0500

    Advances

---
+++ Challenge_20_java_Christian_Romero_telefono/src/com/krakedev/evaluacion/Directorio.java
@@ -0,0 +1,25 @@
+package com.krakedev.evaluacion;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Directorio {
+    private ArrayList<Contacto> contactos = new ArrayList<>();
+    private Date fechaModificacion;
+
+    public boolean agregarContacto(Contacto contacto){
+        this.contactos.add(contacto);
+        return true;
+    }
+
+    public Contacto buscarPorCedula(String cedula){
+        if(this.contactos.size()>=0){
+            for(Contacto c: contactos){
+                if(c.getCedula().equals(cedula)){
+                    return c;
+                }
+            }
+        }
+        return null;
+    }
+}
