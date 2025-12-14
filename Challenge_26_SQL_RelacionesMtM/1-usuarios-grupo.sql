ef9db3dbffc8bd5d39eeb3a54107b33342d93634
Christian R <cjromerom28@gmail.com>
2025-12-14 14:06:11 -0500

    ADvacnes

--- Challenge_26_SQL_RelacionesMtM/1-usuarios-grupo.sql
+++ Challenge_26_SQL_RelacionesMtM/1-usuarios-grupo.sql
@@ -21,3 +21,65 @@ CREATE TABLE usuario_grupo (
 );
 
 --Agregar la Data.sql antes de las sigueintes consultas
+--=============
+--CONSULTA 1
+--=============
+--Consulta
+select usuarios.nombre, grupo.nombre from usuarios, usuario_grupo, grupo
+where usuarios.id=usuario_grupo.ud_id
+and grupo.id=usuario_grupo.gr_id
+
+--Subconsulta
+select * from usuarios
+where id in (
+	select ud_id from usuario_grupo
+	where gr_id = 1
+)
+
+--Agregacion
+select grupo.nombre, count(usuario_grupo.ud_id) from grupo, usuario_grupo
+where grupo.id=usuario_grupo.gr_id
+group by grupo.nombre
+
+--=============
+----CONSULTA 2
+--=============
+--Consulta
+select usuarios.nombre, grupo.nombre from usuarios, grupo, usuario_grupo
+where usuarios.id=usuario_grupo.ud_id
+and grupo.id=usuario_grupo.gr_id
+and grupo.nombre like '%intensivo%'
+
+--Subconsulta
+select nombre from usuarios
+where id in(
+	select ud_id from usuario_grupo
+	where gr_id=2
+)
+
+--Agregacion
+select grupo.nombre, max(usuario_grupo.ud_id) from grupo, usuario_grupo
+where grupo.id=usuario_grupo.gr_id
+group by grupo.nombre
+
+--=============
+----CONSULTA 3
+--=============
+--Consulta
+select usuarios.nombre, grupo.fecha_creacion from usuarios, grupo, usuario_grupo
+where usuarios.id=usuario_grupo.ud_id
+and grupo.id=usuario_grupo.gr_id
+and grupo.fecha_creacion between '2020-03-08' and '2022-03-08'
+
+--Subconsulta
+select nombre from usuarios
+where id in(
+	select Ud_id from usuario_grupo
+	where gr_id=3
+)
+
+--Agregacion
+select grupo.descripcion, count(usuario_grupo.ud_id) from grupo, usuario_grupo
+where grupo.id=usuario_grupo.gr_id
+and grupo.descripcion like '%matutino%'
+group by grupo.descripcion
--- Challenge_26_SQL_RelacionesMtM/2-habitaciones-huespedes.sql
+++ Challenge_26_SQL_RelacionesMtM/2-habitaciones-huespedes.sql
@@ -27,3 +27,63 @@ CREATE TABLE reservas (
 );
 
 --Agregar la Data.sql antes de las sigueintes consultas
+--=============
+--CONSULTA 1
+--=============
+--Consulta
+select habitaciones.habitacion_numero, huespedes.nombres, huespedes.apellidos from habitaciones, huespedes, reservas
+where habitaciones.habitacion_numero=reservas.habitacion
+and huespedes.id=reservas.huespedes_id;
+
+--Subconsulta
+select nombres, apellidos from huespedes
+where id in (
+	select huespedes_id from reservas
+	where habitacion=2
+);
+
+--Agregacion
+select habitaciones.habitacion_numero, count(reservas.huespedes_id) from habitaciones, reservas
+where habitaciones.habitacion_numero=reservas.habitacion
+group by habitaciones.habitacion_numero;
+
+--=============
+--CONSULTA 2
+--=============
+--Consulta
+select habitaciones.habitacion_numero, habitaciones.piso, huespedes.nombres, huespedes.apellidos from habitaciones, huespedes, reservas
+where habitaciones.habitacion_numero=reservas.habitacion
+and huespedes.id=reservas.huespedes_id
+and habitaciones.piso=4;
+
+--Subconsulta
+select nombres, apellidos from huespedes
+where id in(
+	select huespedes_id from reservas
+	where habitacion=3
+);
+
+--Agregacion
+select habitaciones.habitacion_numero, avg(reservas.huespedes_id) from habitaciones, reservas
+where habitaciones.habitacion_numero=reservas.habitacion
+group by habitaciones.habitacion_numero;
+
+--=============
+--CONSULTA 3
+--=============
+--Consulta
+select habitaciones.habitacion_numero, huespedes.nombres, huespedes.apellidos from habitaciones, huespedes, reservas
+where habitaciones.habitacion_numero=reservas.habitacion
+and huespedes.id=reservas.huespedes_id;
+
+--Subconsulta
+select nombres, apellidos from huespedes
+where id in(
+	select huespedes_id from reservas
+	where habitacion=4
+);
+
+--Agregacion
+select habitaciones.habitacion_numero, sum(habitaciones.precio_por_noche) from habitaciones, reservas
+where habitaciones.habitacion_numero=reservas.habitacion
+group by habitaciones.habitacion_numero;
