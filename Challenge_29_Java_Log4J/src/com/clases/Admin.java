da0556132a273629a28f8407a9415f1969491e91
Christian R <cjromerom28@gmail.com>
2025-12-14 21:55:14 -0500

    Advacnes

--- Challenge_29_Java_Log4J/src/com/clases/Admin.java
+++ Challenge_29_Java_Log4J/src/com/clases/Admin.java
@@ -6,7 +6,10 @@ import org.apache.logging.log4j.Logger;
 public class Admin {
     private static Logger logger = LogManager.getLogger(Admin.class);
     public void agregar(){
-        logger.info("Ingresa agregar");
+        logger.info("Mensaje Info");
+        logger.warn("Mensaje Warn");
+        logger.trace("Mensaje Trace");
+        logger.error("Mensaje Error");
+        logger.debug("Mensaje Debug");
     }
-
 }
