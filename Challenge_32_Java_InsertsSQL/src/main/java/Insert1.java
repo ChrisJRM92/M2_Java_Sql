39b6f28add3021166b653d15c0498557bc895924
Christian R <cjromerom28@gmail.com>
2025-12-15 18:51:42 -0500

    Advances

--- Challenge_32_Java_InsertsSQL/src/main/java/Insert1.java
+++ Challenge_32_Java_InsertsSQL/src/main/java/Insert1.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Insert1 {
+    public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/relaciones_db", "postgres", "12345");
+            System.out.println("Conexion exitosa!");
+            ps = connection.prepareStatement("insert into clientes(cedula, nombre, apellido)"+"values(?,?,?)");
+            ps.setString(1, "070107112");
+            ps.setString(2, "Sofia");
+            ps.setString(3, "Romero");
+            //ps.setInt();
+            //ps.setDouble();
+
+            ps.executeUpdate();
+            System.out.println("Datos agregados...");
+        }catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
