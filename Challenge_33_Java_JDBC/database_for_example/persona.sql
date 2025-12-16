bf229b222783bc85f2fd9a9fe600658305d8c486
Christian R <cjromerom28@gmail.com>
2025-12-16 10:00:45 -0500

    Create database for example

---
+++ Challenge_33_Java_JDBC/database_for_example/persona.sql
@@ -0,0 +1,21 @@
+--Primero crear la base de datos para empezar a operar
+CREATE TABLE estado_civil(
+    codigo char(1) PRIMARY KEY NOT NULL,
+    descripcion varchar(20) NOT NULL
+);
+
+CREATE TABLE persona(
+    cedula char(10) PRIMARY KEY NOT NULL,
+    nombre varchar(50) NOT NULL,
+    apellido varchar(50) NOT NULL,
+    estatura decimal(10,2) NOT NULL,
+    fecha_nacimiento date NOT NULL,
+    hora_nacimiento time,
+    cantidad_ahorrada money,
+    estado_civil char(1) NOT NULL,
+    numero_hijos int,
+    FOREIGN KEY (estado_civil) REFERENCES estado_civil(codigo)
+);
+
+
+
