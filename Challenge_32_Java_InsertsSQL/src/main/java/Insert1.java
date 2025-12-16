51da612b8dc3ded3bf1755575e9414f9a3c5e4a7
Christian R <cjromerom28@gmail.com>
2025-12-15 20:16:46 -0500

    Advances

--- Challenge_32_Java_InsertsSQL/src/main/java/Insert1.java
+++ Challenge_32_Java_InsertsSQL/src/main/java/Insert1.java
@@ -2,24 +2,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
 
 public class Insert1 {
     public static void main(String[] args) {
         Connection connection = null;
         PreparedStatement ps = null;
+        BigDecimal estatura = new BigDecimal("1.75");
+        Date fechaNacimiento = Date.valueOf("1992-02-28");
+        Time horaNacimiento = Time.valueOf("14:35:00");
+        BigDecimal cantidadAhorrada = new BigDecimal("1250.50");
         try{
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/relaciones_db", "postgres", "12345");
             System.out.println("Conexion exitosa!");
-            ps = connection.prepareStatement("insert into clientes(cedula, nombre, apellido)"+"values(?,?,?)");
-            ps.setString(1, "070107112");
-            ps.setString(2, "Sofia");
+            ps = connection.prepareStatement("insert into persona(cedula, nombre, apellido, estatura, fecha_nacimiento, hora_nacimiento, cantidad_ahorrada, numero_hijos)"+"values(?,?,?,?,?,?,?,?)");
+            ps.setString(1, "0705933588");
+            ps.setString(2, "Christian");
             ps.setString(3, "Romero");
-            //ps.setInt();
-            //ps.setDouble();
+            ps.setBigDecimal(4, estatura);
+            ps.setDate(5, fechaNacimiento);
+            ps.setTime(6, horaNacimiento);
+            ps.setBigDecimal(7, cantidadAhorrada);
+            ps.setInt(8, 2);
 
             ps.executeUpdate();
-            System.out.println("Datos agregados...");
+            System.out.println("Datos agregados correctamente...");
         }catch(ClassNotFoundException | SQLException e){
             e.printStackTrace();
         }
--- Challenge_32_Java_InsertsSQL/src/main/java/Insert2.java
+++ Challenge_32_Java_InsertsSQL/src/main/java/Insert2.java
@@ -1,5 +1,32 @@
+import java.math.BigDecimal;
+import java.sql.*;
+
 public class Insert2 {
     public static void main(String[] args) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        BigDecimal estatura = new BigDecimal("1.60");
+        Date fechaNacimiento = Date.valueOf("1993-05-15");
+        Time horaNacimiento = Time.valueOf("08:45:30");
+        BigDecimal cantidadAhorrada = new BigDecimal("11250.50");
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/relaciones_db", "postgres", "12345");
+            System.out.println("Conexion exitosa!");
+            ps = connection.prepareStatement("insert into persona(cedula, nombre, apellido, estatura, fecha_nacimiento, hora_nacimiento, cantidad_ahorrada, numero_hijos)"+"values(?,?,?,?,?,?,?,?)");
+            ps.setString(1, "070701119");
+            ps.setString(2, "Sofia");
+            ps.setString(3, "Romero");
+            ps.setBigDecimal(4, estatura);
+            ps.setDate(5, fechaNacimiento);
+            ps.setTime(6, horaNacimiento);
+            ps.setBigDecimal(7, cantidadAhorrada);
+            ps.setInt(8, 3);
 
+            ps.executeUpdate();
+            System.out.println("Datos agregados correctamente...");
+        }catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
     }
 }
