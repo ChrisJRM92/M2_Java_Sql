19a143f4632f46ac9aa257f130303f38fd5f40af
Christian R <cjromerom28@gmail.com>
2025-12-16 09:41:06 -0500

    Advances

---
+++ Challenge_33_Java_JDBC/src/main/java/com/entidades/Persona.java
@@ -0,0 +1,16 @@
+package com.entidades;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Persona {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    //private int estadocivil;
+    private double estatura;
+    private Date fechaNacimiento;
+    private Date horaNacimiento;
+    private BigDecimal cantidadAhorrada;
+    private int numeroHijos;
+}
