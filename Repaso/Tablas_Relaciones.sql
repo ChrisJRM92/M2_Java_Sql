084e13955ac1361bcc769a86168f88df98872812
Christian R <cjromerom28@gmail.com>
2025-12-19 16:10:17 -0500

    Advances

--- Repaso/Tablas_Relaciones.sql
+++ Repaso/Tablas_Relaciones.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE repasoDB;
 
+drop table if exist historial_movimientos;
+drop table if exist productos;
+drop table if exist categorias;
+
 --En postgres el autoincrement es SERIAL.
 CREATE TABLE categorias(
     id SERIAL PRIMARY KEY,
@@ -25,10 +29,6 @@ CREATE TABLE historial_movimientos(
 
 --Insertar la data
 --Categorias
-insert into categorias(nombre) VALUES('Bebidas');
-insert into categorias(nombre) VALUES('Snaks');
-insert into categorias(nombre) VALUES('Golosinas');
-
 insert into categorias(nombre) VALUES
 ('Bebidas'),
 ('Snacks'),
@@ -38,8 +38,18 @@ DELETE FROM categorias WHERE id = 1;
 
 --Productos
 insert into productos(nombre, precio_venta, precio_compra, id_categoria) VALUES
-('Coca cola 300ml', 0.7, 0.55, 4),
-('Coca cola 1000ml', 1, 0.80, 4),
-('Doritos 50g', 0.5, 0.42, 5),
-('Manicho', 0.25, 0.21, 6),
-('Tango', 0.5, 0.42, 6);
+('Coca cola 300ml', 0.7, 0.55, 1),
+('Coca cola 1000ml', 1, 0.80, 1),
+('Doritos 50g', 0.5, 0.42, 2),
+('Manicho', 0.25, 0.21, 3),
+('Tango', 0.5, 0.42, 3);
+
+--Historial
+INSERT INTO historial_movimientos(id_producto, cantidad, fecha_movimiento) VALUES
+(1, 10, '2020/10/28 09:45'),
+(2, -3, '2020/10/28 10:49'),
+(1, 5, '2020/10/28 12:23');
+
+select * from productos;
+select * from categorias;
+select * from historial_movimientos;
